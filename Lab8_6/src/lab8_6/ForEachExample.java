3e39ea21ff340c6447b58e8d4b792c937a57dcbb
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-10-15 16:17:54 -0500

    update: Done Lab8_6 regarding method referencing

---
+++ Lab8_6/src/lab8_6/ForEachExample.java
@@ -0,0 +1,14 @@
+package lab8_6;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ForEachExample {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", "Away", "On Vacation", "Everywhere you want to be");
+		list.forEach(s -> System.out.println(s.toUpperCase()));
+		System.out.println("");
+		list.stream().map(String::toUpperCase).forEach(System.out::println);
+	}
+}
